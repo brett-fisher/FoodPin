ea6d880421da642934174989d72670ce96ec6f2c
Brett Fisher <bdf5106@psu.edu>
2017-10-18 00:23:04 -0400

    A few initial changes to the storyboard.

---
+++ FoodPin/Controllers/RestaurantTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RestaurantTableViewController.swift
+//  FoodPin
+//
+//  Created by Brett Fisher on 10/18/17.
+//  Copyright © 2017 Brett Fisher. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
