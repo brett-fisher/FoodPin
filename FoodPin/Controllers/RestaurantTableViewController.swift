87b0a438c4976e376d788c7e86c1106924bf4578
Brett Fisher <bdf5106@psu.edu>
2017-10-18 00:33:02 -0400

    Added stuff to the table view.

--- FoodPin/Controllers/RestaurantTableViewController.swift
+++ FoodPin/Controllers/RestaurantTableViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 
 class RestaurantTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant",
+                        "Po's Atlier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Express", "Upstate",
+                        "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional",
+                        "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
 
-    // MARK: - Table view data source
+  // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return restaurantNames.count
+  }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    return cell
+  }
 }
