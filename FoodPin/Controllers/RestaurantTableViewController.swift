b2c309d2e230239715ab2317167422d29611a627
Brett Fisher <bdf5106@psu.edu>
2017-10-19 20:59:29 -0400

    Made lots of changes to the UI

--- FoodPin/Controllers/RestaurantTableViewController.swift
+++ FoodPin/Controllers/RestaurantTableViewController.swift
@@ -14,6 +14,11 @@ class RestaurantTableViewController: UITableViewController {
                         "Po's Atlier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Express", "Upstate",
                         "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional",
                         "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+  var restaurantImageNames = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant",
+                                "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate",
+                                "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional",
+                                "barrafina", "donostia", "royaloak", "caskpubkitchen"]
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -24,16 +29,20 @@ class RestaurantTableViewController: UITableViewController {
   }
 
   // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return restaurantNames.count
   }
 
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RestaurantTableViewCell
     
-    cell.textLabel?.text = restaurantNames[indexPath.row]
-    cell.imageView?.image = UIImage(named: "restaurant")
+    cell.nameLabel?.text = restaurantNames[indexPath.row]
+    cell.thumbnailImageView?.image = UIImage(named: restaurantImageNames[indexPath.row])
     
     return cell
   }
---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Brett Fisher on 10/19/17.
+//  Copyright © 2017 Brett Fisher. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+  
+  
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var typeLabel: UILabel!
+  @IBOutlet var locationLabel: UILabel!
+  @IBOutlet weak var thumbnailImageView: UIImageView!
+  
+  
+//    @IBOutlet var restaurantName: UILabel?
+//    @IBOutlet var restaurantLocation: UILabel?
+//    @IBOutlet var restaurantType: UILabel?
+//    @IBOutlet var thumbnailImageView: UIImageView?
+  
+}
